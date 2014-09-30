1aca8ce2bd6503596a627119a562c1b1325f9056
FND <fnd@localhost.localdomain>
2014-09-30 14:15:05 +0200

    added support for JavaScript transpilers

--- samples/gulpfile.js
+++ samples/gulpfile.js
@@ -5,21 +5,26 @@ var sass = require("feta/tasks/sass"); // NB: LESS works the same way
 var browserify = require("feta/tasks/browserify");
 var jscs = require("feta/tasks/jscs");
 var jshint = require("feta/tasks/jshint");
+var path = require("path");
 
 var paths = {
 	sassEntry: "styles/main.scss",
 	sassAll: "styles/**/*.scss",
 	css: "bundle.css",
 	jsEntry: "scripts/main.js",
+	csEntry: "scripts/main.coffee", // unused
 	jsAll: "scripts/**/*.js",
 	js: "bundle.js",
 	distro: "dist"
 };
 
+var jsExtensions = [".coffee"]; // unused
+
 gulp.task("autocompile", ["styles"], function() {
 	gulp.watch(paths.sassAll, ["styles"]);
 	gulp.watch(paths.jsAll, ["lint"]);
-	browserify.watchify(paths.jsEntry);
+	browserify.watchify(paths.jsEntry, path.join(paths.distro, paths.js),
+			jsExtensions);
 	// TODO: return value?
 });
 
@@ -27,7 +32,8 @@ gulp.task("lint", ["jshint", "jscs"]);
 
 gulp.task("styles", sass(paths.sassEntry, paths.css, paths.distro));
 
-gulp.task("browserify", browserify(paths.jsEntry));
+gulp.task("browserify", browserify(paths.jsEntry,
+		path.join(paths.distro, paths.js), jsExtensions));
 
 gulp.task("jscs", jscs(paths.jsAll));
 
--- tasks/browserify.js
+++ tasks/browserify.js
@@ -4,29 +4,42 @@ var gulp = require("gulp");
 
 // `entryPoint` is a JavaScript file
 // `target` is the bundle's file path
-module.exports = function(entryPoint, target) {
+// `extensions` is a list of file extensions to be supported (e.g.
+// NB: transforms (e.g. preprocessors) should be declared in `package.json`
+// XXX: `extensions` should eventually be managed via `package.json` as well:
+// https://github.com/substack/node-browserify/issues/809
+module.exports = function(entryPoint, target, extensions) {
 	return function() { // gulp task
-		bundle(entryPoint, target);
+		bundle(entryPoint, target, { extensions: extensions });
 		// TODO: return value?
 	};
 };
 
 // arguments are the same as above
 // NB: not a gulp task
-module.exports.watchify = function(entryPoint, target) {
-	bundle(entryPoint, target, true);
+module.exports.watchify = function(entryPoint, target, extensions) {
+	var options = {
+		extensions: extensions,
+		watch: true
+	};
+	bundle(entryPoint, target, options);
 	// TODO: return value?
 };
 
 // TODO: blank bundle on error
-function bundle(entryPoint, target, watch) {
+function bundle(entryPoint, target, options) {
+	options = options || {};
+	options.extensions = options.extensions || [];
 	var argv = [null, null];
 
 	var module = "browserify/bin/cmd";
-	if(watch) {
+	if(options.watch) {
 		module = "watchify/bin/cmd";
 		argv.push("-v");
 	}
+	options.extensions.forEach(function(ext) {
+		argv.push("--extension=" + ext); // XXX: don't use `=`!?
+	});
 
 	argv.push("-o", target, entryPoint);
 	process.argv = argv;
